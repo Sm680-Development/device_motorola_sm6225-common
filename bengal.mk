c36620a7161e30dbce783c4256becd57037d8ddd
Madhanraj Chelladurai <quic_mchellad@quicinc.com>
2023-09-01 18:22:06 +0200

    sm6225-common: add boot animation configs

--- bengal.mk
+++ bengal.mk
@@ -7,6 +7,11 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # A/B
 AB_OTA_UPDATER := true
 
